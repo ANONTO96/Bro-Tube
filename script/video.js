055854641552f9d91021f3777de1f3b1af7465f7
ANONTO <tanjirulanonto04@gmail.com>
2024-10-03 16:32:08 +0600

    date set done

--- script/video.js
+++ script/video.js
@@ -1,3 +1,13 @@
+// global function 
+function getTimeString(time){
+    // get hour and rest sec
+    const hour = parseInt(time / 3600);
+    let remainSecond = time % 3600;
+    // get minute adn rest sec
+    const minute = parseInt(remainSecond / 60);
+    remainSecond = remainSecond % 60;
+    return `${hour} hour ${minute} minute ${remainSecond} second ago`;
+}
 // fetch,load and show category on html
 
 // create load categories
@@ -41,10 +51,14 @@ const displayVideos = (videos) => {
         const card = document.createElement('div');
         card.classList = "h-[260px]"
         card.innerHTML = `
-    <figure class = "h-[200px]">
+    <figure class = "h-[200px] relative">
     <img class = "w-full h-full object-cover rounded-t-2xl "
-      src= ${video.thumbnail}
+    src= ${video.thumbnail}/>
+    ${
+        video.others.posted_date?.length == 0 ? "" : `<span class = "absolute right-2 bottom-2 text-white bg-black rounded p-1">${getTimeString(video.others.posted_date)} </span>`
+    }
     </figure>
+    
     <div class="px-0 py-3 flex items-center gap-2">
      <div>
       <img class = "w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}"/>
@@ -53,7 +67,8 @@ const displayVideos = (videos) => {
      <h2 class = "font-bold">${video.title}</h2>
      <div class = "flex items-center gap-2">
       <p class = "text-gray-400">${video.authors[0].profile_name}</p>
-      ${video.authors[0].verified === true ? '<img class = "w-5 h-5 "  src = "https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000"/>' : ""
+      ${
+        video.authors[0].verified === true ? '<img class = "w-5 h-5 "  src = "https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000"/>' : ""
       }
      </div>
      </div>
